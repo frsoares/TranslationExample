73c058c0f727f5bccb46ee7c5b4e0c2a5c543dde
Francisco Soares <fmssn@cin.ufpe.br>
2025-09-22 17:01:10 -0300

    Initial Flashcard implementation. Includes Translation.

--- TranslationExample/FlashCardView.swift
+++ TranslationExample/FlashCardView.swift
@@ -6,10 +6,41 @@
 //
 
 import SwiftUI
+import Translation
 
 struct FlashCardView: View {
+
+    @State var verseText: String = "When I find myself in times of trouble, Mother Mary comes to me"
+    @State var translationText: String = ""
+
+    var sourceLanguage: Locale.Language? = Locale.Language(identifier: "en-US")
+    var targetLanguage: Locale.Language? = Locale.Language(identifier: "pt-BR")
+
+    @State var showResult: Bool = false
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text(showResult ? translationText : verseText)
+                .translationTask(
+                    source: sourceLanguage,
+                    target: targetLanguage
+                ) { session in
+                    do {
+                        let response = try await session.translate(verseText)
+                        translationText = response.targetText
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+                .foregroundStyle(.black)
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+            }
+            Button("Translate") {
+                showResult = true
+            }
+        }
     }
 }
 
--- TranslationExample/TranslationExampleApp.swift
+++ TranslationExample/TranslationExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TranslationExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FlashCardView()
         }
     }
 }
