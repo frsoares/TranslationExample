3b51e33ba4cab0a4b16af41d2fe7c4a4aec51871
Francisco Soares <fmssn@cin.ufpe.br>
2025-09-22 16:22:49 -0300

    Initial Commit

---
+++ TranslationExample/TranslationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationExampleApp.swift
+//  TranslationExample
+//
+//  Created by Francisco Miranda Soares on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
