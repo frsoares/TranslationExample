4ffc0b69989389db1b3e91530b82734c7e3e0a96
daviaugusto222 <daviaugusto222@gmail.com>
2025-09-23 09:16:36 -0300

    Implementação inicial Lista de músicas

--- TranslationExample/SongsView.swift
+++ TranslationExample/SongsView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+struct Song: Identifiable {
+    let id = UUID()
+    let title: String
+    let artist: String
+    let cardsCount: Int
+}
+
 struct SongsView: View {
+    let songs = [
+        Song(title: "Let It Be", artist: "Beatles", cardsCount: 6),
+        Song(title: "Shallow", artist: "Lady Gaga", cardsCount: 8),
+        Song(title: "What Is This Feeling?", artist: "Ariana Grande and Cynthia Erivo", cardsCount: 12)
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 16) {
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(songs) { song in
+                        SongCard(song: song)
+                    }
+                }
+                .padding()
+            }
+        }
+        .background(Color(.systemMint))
+    }
+}
+
+struct SongCard: View {
+    let song: Song
+    
+    var body: some View {
+        VStack(spacing: 6) {
+            Text("\(song.cardsCount) cartas")
+                .font(.caption)
+                .italic()
+                .foregroundColor(.black.opacity(0.7))
+            
+            Text("\(song.title) - \(song.artist)")
+                .font(.body)
+                .foregroundColor(.black)
+        }
+        .frame(maxWidth: .infinity, minHeight: 100)
+        .background(Color(.white))
+        .cornerRadius(16)
     }
 }
 
